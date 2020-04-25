73efb5679cdfe38a13afe71dd8f4496919a8bce3
Aishwarya K P <64296803+AishwaryaKP@users.noreply.github.com>
2020-04-25 20:35:48 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -20,9 +20,6 @@ public class Student
 	@Column(name="marks")
     private int marks;
 	
-	
-	
-	
 	public Student(int roll, String name, int marks) {
 		super();
 		this.roll = roll;
@@ -30,73 +27,33 @@ public class Student
 		this.marks = marks;
 	}
 
-	
-	
-
-
-
 	public int getRoll() {
 		return roll;
 	}
-
-
-
-
-
 
 	public void setRoll(int roll) {
 		this.roll = roll;
 	}
 
-
-
-
-
-
 	public String getName() {
 		return name;
 	}
-
-
-
-
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
-
-
-
-
 	public int getMarks() {
 		return marks;
 	}
-
-
-
-
-
 
 	public Student() {
 		super();
 	}
 
-
-
-
-
-
 	public void setMarks(int marks) {
 		this.marks = marks;
 	}
-
-
-
-
-
 
 	@Override
 	public String toString() 
